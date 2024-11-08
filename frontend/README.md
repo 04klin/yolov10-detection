93ff5c224d0fef8d8a3fd5e79cfb7634d6fd4d2d
Kevin Lin <2004klin@gmail.com>
2024-11-08 03:37:04 -0500

    Upload react template and integrate yolov10 placeholder

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+## Frontend
+
+| React |
+| ----- |
+
+| [![Material Dashboard React](https://s3.amazonaws.com/creativetim_bucket/products/71/thumb/material-dashboard-react.jpg?1638950990)](http://demos.creative-tim.com/material-dashboard-react/#/dashboard?ref=readme-mdr)
+
+The documentation for the Material Dashboard is hosted at this [website](https://www.creative-tim.com/learning-lab/react/overview/material-dashboard/?ref=readme-mdr).
---
+++ frontend/src/layouts/dashboard/index.js
@@ -0,0 +1,37 @@
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import Detector from "./components/Detector";
+// Dashboard components
+
+function Dashboard() {
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox py={3}>
+        <MDBox>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <Detector />
+              {/* Add drag and drop here */}
+            </Grid>
+            <Grid item xs={12} md={6} lg={8}>
+              {/* Add drag and drop here */}
+            </Grid>
+          </Grid>
+        </MDBox>
+      </MDBox>
+      {/* <Footer /> */}
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
